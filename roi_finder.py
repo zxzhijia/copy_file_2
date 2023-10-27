557a7e70f59e44f644da2ffa86f520d1a92f5d07
Xu123 <xz70@duke.edu>
2023-10-27 11:04:07 -0700

    Update roi_finder.py

--- roi_finder.py
+++ roi_finder.py
@@ -1,41 +1,39 @@
 import cv2
 import numpy as np
 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
 def select_roi(image_path):
-    # Read the image with OpenCV
+    # Read the image
     img = cv2.imread(image_path)
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_copy = img.copy()
 
-    # Use Matplotlib to visualize the image and select the ROI
-    fig, ax = plt.subplots(1)
-    ax.imshow(img_rgb)
+    # Resize the image for easier selection (4 times larger)
+    img_resized = cv2.resize(img, (img.shape[1]*4, img.shape[0]*4))
 
-    roi = plt.ginput(2)  # User selects two points (top-left and bottom-right)
-    plt.close()
+    # Let the user select the ROI on the resized image
+    roi_resized = cv2.selectROI("Select ROI", img_resized, False, False)
+    cv2.destroyAllWindows()
 
-    # Convert the selected ROI coordinates to integer format
-    roi_coords = [(int(roi[0][0]), int(roi[0][1])), (int(roi[1][0]), int(roi[1][1]))]
-    cropped_roi = img[roi_coords[0][1]:roi_coords[1][1], roi_coords[0][0]:roi_coords[1][0]]
+    # Convert the ROI coordinates back to the original image size
+    roi = (roi_resized[0]//4, roi_resized[1]//4, roi_resized[2]//4, roi_resized[3]//4)
+
+    # Crop the selected ROI from the original image
+    cropped_roi = img_copy[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
     return img, cropped_roi
 
 def find_similar_rois(img, cropped_roi):
     # Match the ROI throughout the image
     result = cv2.matchTemplate(img, cropped_roi, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8  # Adjust this threshold as needed
+    threshold = 0.8  # You can adjust this threshold as needed
     loc = np.where(result >= threshold)
-
+    
     for pt in zip(*loc[::-1]):
         cv2.rectangle(img, pt, (pt[0] + cropped_roi.shape[1], pt[1] + cropped_roi.shape[0]), (0, 0, 255), 2)
-
+    
     cv2.imshow('Detected ROIs', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
+    
 if __name__ == "__main__":
     image_path = "path_to_your_image.jpg"
     img, cropped_roi = select_roi(image_path)
