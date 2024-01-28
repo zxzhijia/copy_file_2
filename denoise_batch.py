27e16eb3fae14162136ce9a1b05d1313227068b7
Xu123 <xz70@duke.edu>
2024-01-28 10:04:08 -0800

    Create denoise_batch.py

---
+++ denoise_batch.py
@@ -0,0 +1,36 @@
+import os
+import json
+import subprocess
+
+def run_denoise_util(input_dir, output_dir, noise_level):
+    cmd = [
+        "python", "denoise_util.py",
+        "--input_image_format", ".tif",
+        "--output_image_format", ".tif",
+        "--input_directory", input_dir,
+        "--output_directory", output_dir,
+        "--params_json", "data.json",
+        "--new_binary", "False"
+    ]
+    subprocess.run(cmd)
+
+def update_noise_level(noise_level):
+    with open('data.json', 'r+') as file:
+        data = json.load(file)
+        data['noise_level'] = noise_level
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+def process_subfolders(parent_folder):
+    for subdir, dirs, files in os.walk(parent_folder):
+        if any(file.endswith('.tif') for file in files):
+            for noise_level in [3, 5, 7]:
+                output_subdir = os.path.join('..', 'Image', 'Output', f'{os.path.basename(subdir)}_noise_{noise_level}')
+                os.makedirs(output_subdir, exist_ok=True)
+                update_noise_level(noise_level)
+                run_denoise_util(subdir, output_subdir, noise_level)
+
+if __name__ == "__main__":
+    parent_folder = "./../Image/Input"  # Change this to your parent folder path
+    process_subfolders(parent_folder)
